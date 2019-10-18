bcaa6cea0b7efae4b8aba5410e08e87121351632
larsmarginet <lars.marginet@student.howest.be>
2019-10-18 10:34:06 +0200

    wijzigingen

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
-import "./style.css";
-import { devineLog } from "./js/utility/helpers";
+import './style.css';
 
-devineLog("Hey, ik ben een JS file");
-const arr = [1 , 2,  3];
-
-const ES6Stuff = () => devineLog("Ik kan ES6 stuff aan", ...arr);
-ES6Stuff();
