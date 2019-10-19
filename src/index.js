dfd4eecc93ec223242b317dcc8158053ce5ec3ed
larsmarginet <lars.marginet@student.howest.be>
2019-10-19 12:24:38 +0200

    css klaar, nog niet responsive

--- src/index.js
+++ src/index.js
@@ -2,31 +2,31 @@ import './style.css';
 {
   const init = () => {
     console.log('test');
-    getMenu();
+    // getMenu();
   };
 
-  const getMenu = async () => {
-    const url = './assets/coffees.json';
+  // const getMenu = async () => {
+  //   const url = './assets/coffees.json';
 
-    const response = await fetch(url);
-    parse(await response.json());
-  };
+  //   const response = await fetch(url);
+  //   parse(await response.json());
+  // };
 
-  const parse = coffees => {
-    const list = document.querySelector(`.prices__list`);
-    coffees.forEach(coffee => {
-      const listItem = list.appendChild(document.createElement(`li`));
-      listItem.innerHTML = `
-        <li class="price">
-        <a class="price__button">
-          <span class="price__button__wrapper">
-            <span class="price__button__name">${coffee.name}</span>
-            <span class="price__button__amount">&euro; ${coffee.prices.medium}</span>
-          </span>
-          <span class="price__button__plus">+</span>
-        </a>
-      </li>`;
-    });
-  };
+  // const parse = coffees => {
+  //   const list = document.querySelector(`.prices__list`);
+  //   coffees.forEach(coffee => {
+  //     const listItem = list.appendChild(document.createElement(`li`));
+  //     listItem.innerHTML = `
+  //       <li class="price">
+  //       <a class="price__button">
+  //         <span class="price__button__wrapper">
+  //           <span class="price__button__name">${coffee.name}</span>
+  //           <span class="price__button__amount">&euro; ${coffee.prices.medium}</span>
+  //         </span>
+  //         <span class="price__button__plus">+</span>
+  //       </a>
+  //     </li>`;
+  //   });
+  // };
   init();
 }
